ce692c90b415e1487a4235584d80507c763a9217
Cosmin Poieana <cmin764@gmail.com>
2019-05-18 02:44:36 +0300

    Article card example

--- Chrome/app/scripts.babel/chromereload.js
+++ Chrome/app/scripts.babel/chromereload.js
@@ -32,4 +32,3 @@ connection.onmessage = e => {
     }
   }
 };
-
--- Chrome/app/scripts.babel/popup.js
+++ Chrome/app/scripts.babel/popup.js
@@ -1,1 +1,69 @@
 'use strict';
+
+const BASE_URL = 'https://truestory.one';
+const API_URLS = {
+  'counter_article': '/article/counter',
+  'article_data': '/article/data',
+};
+const API_TOKEN = 'd897703467a4fe7b958b68426f1721dd';
+
+var bgPage = chrome.extension.getBackgroundPage();
+
+
+function log(msg) {
+  bgPage.console.log(msg);
+}
+
+
+function apiUrl(name) {
+  return BASE_URL + '/api' + API_URLS[name];
+}
+
+
+function logApiFailure(xhr) {
+  var msg = xhr.responseJSON.message;
+  bgPage.console.error('Error ' + xhr.status + ': ' + msg);
+}
+
+
+function displayArticle(internalUrl) {
+  $.getJSON({
+    url: internalUrl
+  }).done(function(data) {
+    log(data.article);
+  }).fail(logApiFailure);
+}
+
+
+function getArticles() {
+  chrome.tabs.query(
+    {
+      active: true,
+      lastFocusedWindow: true,
+      currentWindow: true
+    },
+    function (tabs) {
+      var tabUrl = tabs[0].url;
+
+      $.getJSON({
+        url: apiUrl('counter_article'),
+        data: {
+          link: tabUrl
+        }
+      }).done(function(data) {
+        $.each(data.articles, function(idx, article) {
+          displayArticle(article.url);
+        });
+      }).fail(logApiFailure);
+    }
+  );
+}
+
+
+$.ajaxSetup({
+  headers: {
+    'Authorization': 'Bearer ' + API_TOKEN
+  }
+});
+
+$(getArticles);
